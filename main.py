90e1ece36cf92df6e4084275061c0c07efc0256e
João Paulo <joaopwii@gmail.com>
2026-04-24 12:58:27 -0300

    Improving reg_translator

--- main.py
+++ main.py
@@ -1,77 +1,17 @@
 def reg_translator(reg):
-    translator = ["00000","00001","00010","00011","00100","00101","00110","00111",
-     "01000","01001","01010","01011","01100","01101","01110","01111",
-     "10000","10001","10010","10011","10100","10101","10110","10111",
-     "11000","11001","11010","11011","11100","11101","11110","11111"]
-    if reg[0] == "x" and reg[1:].isdigit() and int(reg[1:]) >= 0 and int(reg[1:]) <= 31:
-        return translator[int(reg[1:])]
-    else:
-        if reg == "zero":
-            return translator[0]
-        elif reg == "ra":
-            return translator[1]
-        elif reg == "sp":
-            return translator[2]
-        elif reg == "gp":
-            return translator[3]
-        elif reg == "tp":
-            return translator[4]
-        elif reg == "t0":
-            return translator[5]
-        elif reg == "t1":
-            return translator[6]
-        elif reg == "t2":
-            return translator[7]
-        elif reg == "s0" or reg == "fp":
-            return translator[8]
-        elif reg == "s1":
-            return translator[9]
-        elif reg == "a0":
-            return translator[10]
-        elif reg == "a1":
-            return translator[11]
-        elif reg == "a2":
-            return translator[12]
-        elif reg == "a3":
-            return translator[13]
-        elif reg == "a4":
-            return translator[14]
-        elif reg == "a5":
-            return translator[15]
-        elif reg == "a6":
-            return translator[16]
-        elif reg == "a7":
-            return translator[17]
-        elif reg == "s2":
-            return translator[18]
-        elif reg == "s3":
-            return translator[19]
-        elif reg == "s4":
-            return translator[20]
-        elif reg == "s5":
-            return translator[21]
-        elif reg == "s6":
-            return translator[22]
-        elif reg == "s7":
-            return translator[23]
-        elif reg == "s8":
-            return translator[24]
-        elif reg == "s9":
-            return translator[25]
-        elif reg == "s10":
-            return translator[26]
-        elif reg == "s11":
-            return translator[27]
-        elif reg =="t3":
-            return translator[28]
-        elif reg =="t4":
-            return translator[29]
-        elif reg =="t5":
-            return translator[30]
-        elif reg =="t6":
-            return translator[31]
-        else:
-            raise ValueError("Invalid register name: " + reg)
+    translator = ["zero","ra","sp","gp","tp","t0","t1","t2",
+     "s0","s1","a0","a1","a2","a3","a4","a5",
+     "a6","a7","s2","s3","s4","s5","s6",
+     "s7","s8","s9","s10","s11","t3",
+     "t4","t5","t6"]
+    if reg in translator:
+        return to_bin(translator.index(reg), 5)
+    elif reg[0] == "x" and reg[1:].isdigit() and 0 <= int(reg[1:]) <= 31:
+        return to_bin(int(reg[1:]), 5)
+    elif reg == "fp":
+        return to_bin(8, 5)
+    else:        
+        raise ValueError("Invalid register name: " + reg)
 
 def to_bin(num, bits):
     #Calcula o complemento de 2 para números negativos e retorna a representação binária de num com o número de bits especificado
